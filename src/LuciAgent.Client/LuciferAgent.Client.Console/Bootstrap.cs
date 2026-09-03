46a4d0c304401ba5c7f126640d267dfcef12b148
Lucifer <166632710+thuangf45@users.noreply.github.com>
2026-09-03 19:33:00 +0700

    Configure console client network bootstrap
    Add configurable Address and Port settings to the console bootstrap and register both HTTP and HTTPS client factories. HTTPS now initializes with an SSL context that enables TLS 1.2 and 1.3.

--- src/LuciAgent.Client/LuciferAgent.Client.Console/Bootstrap.cs
+++ src/LuciAgent.Client/LuciferAgent.Client.Console/Bootstrap.cs
@@ -1,12 +1,23 @@
-﻿using LuciferCore.Attributes;
+﻿global using HttpClient = LuciferCore.NetCoreServer.Client.HttpClient;
+using LuciferCore.Attributes;
+using LuciferCore.NetCoreServer.Client;
+using LuciferCore.NetCoreServer.Transport.SSL;
+using System.Security.Authentication;
 
 namespace LuciAgent.Client.Console;
 
 public static class Bootstrap
 {
+    [Config("Address", "127.0.0.1")]
+    private static string Address { get; set; }
+
+    [Config("Port", "23456")]
+    private static int Port { get; set; }
+
     [Bootstrap]
     internal static void Initialize()
     {
-
+        SetModelT<HttpClient>(() => new HttpClient(Address, Port));
+        SetModelT<HttpsClient>(() => new HttpsClient(new SslContext(SslProtocols.Tls12 | SslProtocols.Tls13), Address, Port));
     }
 }
